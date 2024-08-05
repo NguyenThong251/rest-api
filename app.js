9946c2b02f21fec70e13fc3d49becbf028e33435
Nguyễn Hoàng Thông <125713226+NguyenThong251@users.noreply.github.com>
2024-08-05 06:57:22 +0000

    .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const port = 3003;
+const port = process.env.PORT || 4000;
 const cors = require("cors");
 // Cấu hình MongoDB Atlas
 const dbConfig = require("./config");
